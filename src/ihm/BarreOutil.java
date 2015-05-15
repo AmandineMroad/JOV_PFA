b94caff39e3ac505a7e2d566240981f36dc622e4
AmandineMroad <roger.amandine@gmail.com>
2015-05-15 12:49:12 +0200

    chemin relatif jar

--- src/ihm/BarreOutil.java
+++ src/ihm/BarreOutil.java
@@ -35,7 +35,8 @@ public class BarreOutil extends JToolBar{
         super(name);
         
         //ajout des boutons
-        next = createToolBarButton(new ImageIcon(".\\src\\doc\\next.png"),
+        next = createToolBarButton(new ImageIcon(".\\src\\doc\\next.png"),//IDE
+//        next = createToolBarButton(new ImageIcon("..\\src\\doc\\next.png"),//fichier.jar
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -49,7 +50,8 @@ public class BarreOutil extends JToolBar{
                 }, "Passer à l'instruction suivante");
         this.add(next);
         
-        stop = createToolBarButton(new ImageIcon(".\\src\\doc\\stop.png"),
+        stop = createToolBarButton(new ImageIcon(".\\src\\doc\\stop.png"),//IDE
+//        stop = createToolBarButton(new ImageIcon("..\\src\\doc\\stop.png"),//fichier.jar
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -64,7 +66,8 @@ public class BarreOutil extends JToolBar{
                 }, "Aller à la fin et arrêter");
         this.add(stop);
         
-        replay =createToolBarButton(new ImageIcon(".\\src\\doc\\replay.png"),
+        replay =createToolBarButton(new ImageIcon(".\\src\\doc\\replay.png"),//IDE
+//        replay =createToolBarButton(new ImageIcon("..\\src\\doc\\replay.png"),//fichier.jar
                 new Lis_Execute(), "Rejouer depuis le début");
         this.add(replay);
         
--- src/ihm/listeners/Lis_OpenReadMe.java
+++ src/ihm/listeners/Lis_OpenReadMe.java
@@ -22,7 +22,9 @@ public class Lis_OpenReadMe implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if (Desktop.isDesktopSupported()){
             try {
-                Desktop.getDesktop().open(new File(".\\src\\doc\\JOV_ReadMe.txt"));
+                Desktop.getDesktop().open(new File(".\\src\\doc\\JOV_ReadMe.txt")); //IDE
+
+//                Desktop.getDesktop().open(new File("..\\src\\doc\\JOV_ReadMe.txt")); //fichier.jar
             } catch (IOException ex) {
                 System.err.println("Erreur open ReadMe");
                 Logger.getLogger(Lis_OpenReadMe.class.getName()).log(Level.SEVERE, null, ex);
